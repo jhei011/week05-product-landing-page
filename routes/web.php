1c452748b5530a57748889b69e678c947f78fbcc
Student <student@example.com>
2026-09-11 17:13:09 +0800

    feat: create landing page layout and base assets

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+| MP04 - Ember & Oak Coffee Co. Product Landing Page
+*/
+
+Route::view('/', 'pages.home')->name('home');
